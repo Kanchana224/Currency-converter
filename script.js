3978a6e2ab3c93d98e24742724a1bbac11a634e2
Kanchana224 <kkanchana427@gmail.com>
2023-12-02 12:59:56 +0530

    updated

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const input = document.getElementsByClassName("input")[0];
 fetch("https://api.frankfurter.app/currencies")
   .then((res) => res.json())
   .then((res) => dropDown(res));
-
 // Function to populate dropdowns with currency options
 function dropDown(res) {
   let curr = Object.entries(res);
@@ -15,7 +14,7 @@ function dropDown(res) {
  // Append options to both select elements (left and right)
     select[0].innerHTML += opt;
     select[1].innerHTML += opt;
-  }
+  // }
 }
 // Add event listener to the "Convert" button
 button.addEventListener("click", () => {
@@ -29,9 +28,9 @@ button.addEventListener("click", () => {
 // Fetch data for currency conversion
 function convert(curr1, curr2, inputValue) {
   const host = 'api.frankfurter.app';
-  fetch(`https://${host}/latest?amount=${inputValue}&from=${curr1}&to=${curr2}`)
+fetch(`https://${host}/latest?amount=${inputValue}&from=${curr1}&to=${curr2}`)
     .then((resp) => resp.json())
     .then((data) => {
       document.getElementsByClassName("result")[0].value = Object.values(data.rates)[0];
     });
-}
+}}
